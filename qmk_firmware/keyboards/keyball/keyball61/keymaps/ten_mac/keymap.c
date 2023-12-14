8d7d0b559e777db06df10257ca9a1727cd341828
monten.oki <monten.oki@gmail.com>
2023-12-15 00:41:09 +0900

    update keymap ten ten_mac

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/ten_mac/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/ten_mac/keymap.c
@@ -27,7 +27,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,    KC_Q,    KC_W,      KC_E,      KC_R,      KC_T,                             KC_Y,      KC_U,      KC_I,      KC_O,      KC_P,      KC_BSLS,
     MO(1),     KC_A,    KC_S,      KC_D,      KC_F,      KC_G,                             KC_H,      KC_J,      KC_K,      KC_L,      KC_SCLN,   KC_QUOT,
     KC_LSFT,   KC_Z,    KC_X,      KC_C,      KC_V,      KC_B,     KC_LBRC,     KC_RBRC,   KC_N,      KC_M,      KC_COMM,   KC_DOT,    KC_SLSH,   KC_RSFT,
-    KC_LCTL,   KC_LGUI, MO(2),     KC_LALT,   KC_BTN2,   KC_SPC,	 KC_BTN1,     MO(1),     KC_ENT,                                     KC_MINS,   KC_EQL
+    KC_LCTL,   KC_LALT, MO(2),     KC_LGUI,   KC_BTN2,   KC_SPC,	 KC_BTN1,     MO(1),     KC_ENT,                                     KC_MINS,   KC_EQL
   ),
 
   [1] = LAYOUT(
